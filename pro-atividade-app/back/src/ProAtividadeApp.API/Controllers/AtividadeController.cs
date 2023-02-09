9a65866cfe35cb0fb7b8ac22a24d088ca197456d
pauloosilas <paulo3356@gmail.com>
2023-02-09 16:14:53 -0300

    Parte2-ASP.NET5

---
+++ pro-atividade-app/back/src/ProAtividadeApp.API/Controllers/AtividadeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ProAtividadeApp.API.Models;
+
+namespace ProAtividadeApp.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AtividadeController : ControllerBase
+  {
+
+    public IEnumerable<Atividade> Atividades = new List<Atividade>(){
+                new Atividade(1),
+                new Atividade(2),
+                new Atividade(3),
+            };
+
+    [HttpGet]
+    public IEnumerable<Atividade> Get()
+    {
+      return Atividades;
+    }
+
+
+    [HttpGet("{id}")]
+    public Atividade Get(int id)
+    {
+      return Atividades.FirstOrDefault(Atividade => Atividade.Id == id);
+    }
+
+    [HttpPost]
+    public IEnumerable<Atividade> Post(Atividade atividade)
+    {
+      return Atividades.Append<Atividade>(atividade);
+    }
+
+
+    [HttpPut("{id}")]
+    public Atividade Put(int id, Atividade atividade)
+    {
+      atividade.Id = id + 1;
+      return atividade;
+    }
+
+
+    [HttpDelete]
+    public string Delete()
+    {
+      return "Meu primeiro Delete";
+    }
+  }
+}
---
+++ pro-atividade-app/back/src/ProAtividadeApp.API/Models/Atividade.cs
@@ -0,0 +1,18 @@
+﻿namespace ProAtividadeApp.API.Models
+{
+  public class Atividade
+  {
+    public Atividade()
+    {
+
+    }
+    public Atividade(int id)
+    {
+      this.Id = id;
+    }
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public string Prioridade { get; set; }
+  }
+}
